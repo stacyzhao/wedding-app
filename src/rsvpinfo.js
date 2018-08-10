7a51510c4f1cae6e2ef33e52d45e556e35ce9203
Stacy Zhao <stacyzhao@hotmail.com>
2018-08-10 13:35:50 -0400

    made rsvp into one page

--- src/rsvpinfo.js
+++ src/rsvpinfo.js
@@ -1,100 +1,162 @@
 import React, { Component } from 'react';
 import firebase from './firebase.js';
-import { Redirect } from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
 import Button from './components/Button.js'
 import Select from './components/Select.js'
+import Input from './components/Input.js'
+import Rsvp from './rsvp.js'
+
+function validate(firstName, lastName, isGoing, entree, starter) {
+  const errors = [];
+
+  if (firstName.length === 0) {
+    errors.push("First Name can't be empty");
+  } else if (lastName.length === 0) {
+    errors.push("Last Name can't be empty");
+  } else if (isGoing === '') {
+    errors.push("You havent decided if youre coming yet");
+  }
+
+  return errors;
+}
 
 class RsvpInfo extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      firstName: '',
+      lastName: '',
       starter: '',
       entree: '',
       isGoing: '',
+      moreGuests: [],
       isGoingOptions: ['Accept', 'Regret'],
-      starterOptions: ['Caprese','Salmon'],
-      entreeOptions: ['Duck', 'Steak', 'Gnocchi'],
+      starterOptions: ['Caprese Salad','Cured Salmon'],
+      entreeOptions: ['Duck Breast', 'Steak Frites', 'Wild Mushroom Gnocchi'],
       toCompletePage: false,
-      toMenuSelection: false
+      errors : [],
     };
     this.handleChange = this.handleChange.bind(this);
 
-
     this.handleSubmit = (event) => {
-      const state = this.state;
       event.preventDefault();
+      const state = this.state;
       const guestsRef = firebase.database().ref('guests');
       const guest = {
+        firstName: state.firstName.toUpperCase(),
+        lastName: state.lastName.toUpperCase(),
         isGoing: state.isGoing,
         starter: state.starter,
         entree: state.entree
       }
+      const { firstName, lastName, isGoing } = this.state;
+
+      const errors = validate(firstName, lastName, isGoing);
+      if (errors.length > 0) {
+        this.setState({ errors });
+        return;
+      }
       guestsRef.push(guest);
+
       this.setState({
+        firstName: '',
+        lastName: '',
         isGoing: '',
         starter: '',
         entree: '',
         toCompletePage: true
-      });
+      })
     };
 
     this.update = (event) => {
-      const newState = {};
-      newState[event.target.name] = event.target.value;
-      this.setState(newState);
+      this.setState({[event.target.name]: event.target.value});
     };
   }
+
   handleChange(e) {
-      this.setState({ isGoing: e.target.value , starter: '',
-      entree: ''});
-    }
+    this.setState({
+      isGoing: e.target.value,
+      starter: '',
+      entree: ''
+    });
+  }
+
+  isDisabled() {
+    return this.state.isGoing === 'Regret' || this.state.isGoing === '';
+  }
 
   render() {
-    const { isGoing } = this.state;
+
+    const { errors, isGoing, firstName, lastName, isGoingOptions, starterOptions, entreeOptions } = this.state;
     if (this.state.toCompletePage === true) {
-      return <div className="hero-text"><h2>your info has been sent. Thanks</h2></div>
+      return <div className='hero-text'><h2>your info has been sent. Thanks</h2><h4><Link to='/rsvpmain' > Click here to add more guests</Link></h4></div>
     }
     return (
-      <div className="hero-text">
+      <div className='hero-text'>
         <h2>RSVP</h2>
+        <h4>We hope you can join us!</h4>
+        <h5>Enter your info below:</h5>
         <div>
+          <p></p>
           <form onSubmit={this.handleSubmit}>
-            <label>
-              <div className='div-form'>
+            {errors.map(error => (
+              <p className='error' key={error}>Error: {error}</p>
+            ))}
+            <div className='div-form'>
+              <Input
+                inputtype={'text'}
+                title={'First Name'}
+                name={'firstName'}
+                value={firstName}
+                placeholder={''}
+                onChange={this.update}
+                required
+              />
+              <Input
+                inputtype={'text'}
+                title={'Last Name'}
+                name={'lastName'}
+                value={lastName}
+                placeholder={''}
+                onChange={this.update}
+                required
+              />
+              <Select
+                title={'Will you be able to attend the wedding on Sunday, Sept 23, 2018?'}
+                name={'isGoing'}
+                options={this.state.isGoingOptions}
+                value={isGoing}
+                placeholder={''}
+                handleChange={this.update}
+                required
+              />
+              <fieldset disabled={this.isDisabled()}>
                 <Select
-                  title={"Will you be able to attend the wedding on Sunday, Sept 23, 2018?"}
-                  name={"isGoing"}
-                  options={this.state.isGoingOptions}
-                  value={isGoing}
-                  placeholder={""}
-                  handleChange={this.update}
-                  required
-                />{" "}
-                <Select
-                  title={"Starter"}
-                  name={"starter"}
-                  options={this.state.starterOptions}
+                  title={'Starter'}
+                  name={'starter'}
+                  options={starterOptions}
                   value={this.state.starter}
-                  placeholder={"Select Starter"}
+                  placeholder={'Select Starter'}
                   handleChange={this.update}
-                  disabled={isGoing === 'Regret'}
-                />{" "}
+                  disabled={this.isDisabled()}
+                />
                 <Select
-                  title={"Entree"}
-                  name={"entree"}
-                  options={this.state.entreeOptions}
+                  title={'Entree'}
+                  name={'entree'}
+                  options={entreeOptions}
                   value={this.state.entree}
-                  placeholder={"Select Entree"}
+                  placeholder={'Select Entree'}
                   handleChange={this.update}
-                  disabled={isGoing === 'Regret'}
-                />{" "}
-              </div>
-            </label>
+                  disabled={this.isDisabled()}
+                />
+              </fieldset>
+            </div>
             <Button
               action={this.handleSubmit}
-              type={"primary"}
-              title={"Submit"}
-            />{" "}
+              type={'primary'}
+              title={'Submit'}
+              disabled={!this.state.firstName}
+            />
           </form>
         </div>
       </div>
